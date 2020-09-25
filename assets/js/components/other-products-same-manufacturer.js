d76f56eb7f770abd6ecb9563fee4b2bb94fbc6a8
Elisabeth Asbæk Jeppesen <6605388@RTS-365.dk>
2020-09-25 13:09:59 +0200

    add more javascript for single page

--- assets/js/components/other-products-same-manufacturer.js
+++ assets/js/components/other-products-same-manufacturer.js
@@ -1,10 +1,18 @@
 
 function getSingleProduct(){
 
+    var breadcrumbCategory = document.querySelector(".breadcrumb .breadcrumb__category");
+    var breadcrumbProduct = document.querySelector(".breadcrumb > li:last-of-type");
     var heading = document.querySelector(".overviewSingleProduct__heading");
     var otherProducts = document.querySelector(".overviewSingleProduct__link");
-    var salePrice = document.querySelector(".overviewSingleProduct__price--strike");
-    var price = document.querySelector(".overviewSingleProduct__price");
+    var price = document.querySelector(".overviewSingleProduct__price--strike");
+    var salePrice = document.querySelector(".overviewSingleProduct__price");
+    var description = document.querySelector(".overviewSingleProduct__description");
+
+    /* additional information box: */
+    var manufacturer = document.querySelector(".additionalInfo__manufacturer");
+    var warranty = document.querySelector(".additionalInfo__warranty");
+    /*                             */
     
     var url = new URLSearchParams(window.location.search);
 
@@ -18,12 +26,20 @@ function getSingleProduct(){
 				    return product.name == url.get("name");
                 })
                 
-                heading.innerText = `${result.name}`;
-                salePrice.innerText = `${result.salePrice}`;
-                price.innerText = `${result.price}`;
+                breadcrumbProduct.innerText = `${result.name}`; /* breadcrumb: name of product */
+                heading.innerText = `${result.name}`; /* heading */
+                price.innerText = `${result.price}`; /* price with line through */
+                salePrice.innerText = `${result.salePrice}`; /* price */
+                description.innerText = `${result.description}`; /* description of product */
 
-                otherProducts.innerText = `See other ${result.manufacturer} products`;
+                breadcrumbCategory.innerText = `${result.category}`; /* breadcrumb: category of product */
+                breadcrumbCategory.href = `category-list.html?category=${result.category}`;
+
+                otherProducts.innerText = `See other ${result.manufacturer} products`; /* link for other products from same manufacturer */
                 otherProducts.href = `category-list.html?manufacturer=${result.manufacturer}`;
+
+                manufacturer.innerText = `${result.manufacturer}`; 
+                warranty.innerText = `${result.freeWarranty}`; 
             })
     }
 }
